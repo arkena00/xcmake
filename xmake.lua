9484964d8a83af89932ad065e455b96af36e62dc
SirLynix <lynix680@gmail.com>
2024-01-25 10:34:13 +0100

    Fixes

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,9 @@
 add_rules("mode.debug", "mode.release")
 set_languages("cxx20")
 
-add_requires("tiny-process-library")
+add_requires("fmt", "tiny-process-library")
 
 target("xc")
     add_files("source/**.cpp")
     add_includedirs("include")
-    add_packages("tiny-process-library")
+    add_packages("fmt", "tiny-process-library")
